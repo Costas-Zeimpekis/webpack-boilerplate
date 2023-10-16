5ff9e347c9270fb2aa6e421f0cdf60915d2bb7b1
Costas Zeimpekis <morpheustudio@gmail.com>
2023-10-16 16:45:44 +0300

    feat: webapck boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  entry: { main: path.resolve(__dirname, "src/index.ts") },
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    assetModuleFilename: "[name][ext]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: "Automotation",
+      file: "index.html",
+      template: "src/template.html",
+    }),
+  ],
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
